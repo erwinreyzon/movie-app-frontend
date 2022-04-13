46ca3e2b646a3fd328022e9cf1a0a3b0e0f645eb
erwin <winzon353535@gmail.com>
2022-04-13 08:57:59 -0700

    adds seperate index page

---
+++ src/views/MoviesIndex.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "Movies Index",
+      movies: [],
+    };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/movies.json").then((response) => {
+      this.movies = response.data;
+      console.log("All Movies", this.movies);
+    });
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <div v-for="movie in movies" v-bind:key="movie.id">
+    <p>Title: {{ movie.title }}</p>
+    <p>Year: {{ movie.year }}</p>
+    <p>Director: {{ movie.director }}</p>
+    <p>Plot: {{ movie.plot }}</p>
+  </div>
+</template>
