9ce05f7586b0a407ee93e740b9376aa083b738bb
erwin <winzon353535@gmail.com>
2022-04-19 14:38:50 -0700

    attempts to add sort and search fro index

--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,13 +1,16 @@
 <script>
 import axios from "axios";
+import Vue2Filters from "vue2-filters";
 
 export default {
   data: function () {
     return {
       message: "Movies Index",
       movies: [],
+      // titleFilter: "",
     };
   },
+  mixins: [Vue2Filters.mixin],
   created: function () {
     axios.get("http://localhost:3000/movies.json").then((response) => {
       this.movies = response.data;
@@ -19,6 +22,23 @@ export default {
 
 <template>
   <h1>{{ message }}</h1>
+
+  <!-- these feel broken, they don't work -->
+
+  <!-- Search by title: <input v-model="titleFilter">
+  <div v-for="movie in filterBy(movies, titleFilter, 'title')">
+
+Search by name: <input v-model="titleFilter" list="titles">
+<datalist id="titles">
+	<option v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</option>
+</datalist> -->
+
+  <!-- <div>
+    <button>Sort Alphabetically</button>
+    <div v-for="movie in orderBy(movies, 'title')"></div>
+    <div v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), 'title')"></div>
+  </div> -->
+
   <div v-for="movie in movies" v-bind:key="movie.id">
     <p>Title: {{ movie.title }}</p>
     <p>Year: {{ movie.year }}</p>
